488fc400881722552147247e16d0f7a6906657c0
Sofia Benitez <sofia.benitez39@gmail.com>
2019-06-12 13:38:20 -0400

    adding to cart redux

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {getProduct} from '../store/product'
+import {addingToCart} from '../store/cart'
 import {Navbar, Product} from './index'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
@@ -18,6 +19,19 @@ class SingleProduct extends React.Component {
         <h2>{product.name}</h2>
         <h3>Price: ${product.price}</h3>
         <p> {product.description} </p>
+        <select>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+        <button>Add to Cart</button>
       </div>
     )
   }
@@ -25,7 +39,10 @@ class SingleProduct extends React.Component {
 
 const mapStateToProps = state => ({product: state.product.singleProduct})
 const mapDispatchToProps = dispatch => ({
-  getProduct: id => dispatch(getProduct(id))
+  getProduct: id => dispatch(getProduct(id)),
+  addToCart: (userId, id, quantity) => {
+    dispatch(addingToCart(userId, id, quantity))
+  }
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SingleProduct)
--- client/store/cart.js
+++ client/store/cart.js
@@ -7,9 +7,29 @@ import axios from 'axios'
 
 // Action Type
 const GET_CART = 'GET_CART'
+const ADD_TO_CART = 'ADD_TO_CART'
 
 //Action Creators
 const getCart = items => ({type: GET_CART, items})
+const addToCart = (userId, itemId, quantity) => ({
+  type: ADD_TO_CART,
+  userId,
+  itemId,
+  quantity
+})
+
+export function addingToCart(userId, itemId, quantity) {
+  return async dispatch => {
+    try {
+      const newCartItem = {userId: userId, itemId: itemId, quantity: quantity}
+      const {data} = await axios.post('/api/cart', newCartItem)
+
+      dispatch(addToCart(data))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
 
 //Thunk Creators
 export function getCartThunk() {
@@ -25,12 +45,20 @@ export function getCartThunk() {
 }
 
 //Reducer
-const initialState = []
-export default function(state = initialState, action) {
+
+export default function(state = [], action) {
   switch (action.type) {
     case GET_CART:
       // console.log("action", action);
       return action.items
+    case ADD_TO_CART:
+      const newItem = {
+        userId: action.userId,
+        productId: action.productId,
+        quantity: action.quantity
+      }
+      return [...state, newItem]
+
     default:
       return state
   }
