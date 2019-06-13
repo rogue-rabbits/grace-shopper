734a4bc2dd18ca8e83b14f8696c3cfab9265a91f
Hari.Doshi <hdoshi2@gmail.com>
2019-06-13 17:09:04 -0400

    fix: updated order number generating functionality

--- client/store/orderHistory.js
+++ client/store/orderHistory.js
@@ -3,10 +3,12 @@ import axios from 'axios'
 // Action Type
 const GET_ORDER = 'GET_ORDER'
 const ADD_ORDER = 'ADD_ORDER'
+const UPDATE_LAST_ORDER = 'UPDATE_LAST_ORDER'
 
 //Action Creators
 const getOrder = order => ({type: GET_ORDER, order})
 const addOrder = order => ({type: ADD_ORDER, order})
+export const updateLastOrder = order => ({type: UPDATE_LAST_ORDER, order})
 
 //Thunk Creators
 export function getItemsThunk() {
@@ -32,14 +34,27 @@ export function addItemsThunk(newItem) {
   }
 }
 
+export function getLastOrderThunk() {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get('/api/orderHistory/lastOrder')
+      dispatch(updateLastOrder(data[0].orderNumber))
+    } catch (err) {
+      console.log('error in orderHistory thunk')
+    }
+  }
+}
+
 //Reducer
-const initialState = []
+const initialState = {allOrders: [], lastOrderNum: 0}
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_ORDER:
-      return action.order
+      return {...state, allOrders: action.order}
     case ADD_ORDER:
-      return [...state, action.order]
+      return {...state, allOrders: [...state.allOrders, action.order]}
+    case UPDATE_LAST_ORDER:
+      return {...state, lastOrderNum: action.order}
     default:
       return state
   }
