e35361e2b5da8e6fb52abf8f74363aa02ad508ac
Hari.Doshi <hdoshi2@gmail.com>
2019-06-15 17:03:25 -0400

    refactor: added lodash to arrange order by order number and style

--- client/components/AccountInfo.js
+++ client/components/AccountInfo.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import Card from '@material-ui/core/Paper'
-import Grid from '@material-ui/core/Grid'
+import groupBy from 'lodash/groupBy'
 
 const AccountInfo = props => {
   const {allHistory, currentUser} = props
-  console.log(props)
+  const groupByNo = groupBy(allHistory, 'orderNumber')
+  const orderNumbers = Object.keys(groupByNo)
+
   return (
     <div>
       <div>
@@ -17,28 +19,28 @@ const AccountInfo = props => {
         Address: {currentUser.address1}, {currentUser.city} {currentUser.state},{' '}
         {currentUser.zipCode}
       </div>
-      <Grid container spacing={4} className="cards-container">
-        <div>
-          <br />
-          Order History:
-          {allHistory.map(element => {
-            return (
-              <Card key={element.id}>
-                <div className="card-contents">
-                  Name: {element.name}
-                  <br />
-                  Price: {element.price}
-                  <br />
-                  Quantity: {element.quantity}
-                  <br />
-                  Order #: {element.orderNumber}
-                  <br />
-                </div>
-              </Card>
-            )
-          })}
-        </div>
-      </Grid>
+      <div>
+        <br />
+        Order History:
+        {orderNumbers.map(order => (
+          <Card key={order} className="order-card">
+            <div className="order-heading">Order Number: {order}</div>
+            <div>
+              {groupByNo[order].map(element => {
+                return (
+                  <div key={element.id} className="order-contents">
+                    <div className="order-line1">Name: {element.name}</div>
+                    <div className="order-line2">Price: {element.price}</div>
+                    <div className="order-line2">
+                      Quantity: {element.quantity}
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </Card>
+        ))}
+      </div>
     </div>
   )
 }
