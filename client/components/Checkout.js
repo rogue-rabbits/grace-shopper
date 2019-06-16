6bf3c1ab652a7b2b36b0e2ecb1cca7c81068d11f
Sofia Benitez <sofia.benitez39@gmail.com>
2019-06-16 00:54:06 -0400

    feature: checkout page updated with materials ui api

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getCartThunk, emptyCart} from '../store/cart'
-
 import {addItemsThunk, updateLastOrder} from '../store/orderHistory'
 import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import Paper from '@material-ui/core/Paper'
 
 const defaultState = {
   firstName: '',
@@ -79,81 +81,116 @@ class Checkout extends Component {
     }
 
     return (
-      <form onSubmit={this.handleSubmit} className="checkoutForm">
-        <div>First Name: </div>
-        <input
-          name="firstName"
-          type="text"
-          value={this.state.firstName}
-          onChange={this.handleChange}
-          required
-        />
-        <div>Last Name: </div>
-        <input
-          name="lastName"
-          type="text"
-          value={this.state.lastName}
-          onChange={this.handleChange}
-          required
-        />
-        <div>Address 1: </div>
-        <input
-          name="address1"
-          type="text"
-          value={this.state.address1}
-          onChange={this.handleChange}
-          required
-        />
-        {/* <div>Address 2: </div>
-        <input
-          name="address2"
-          type="text"
-          value={item.address2}
-          onChange={this.handleChange}
-          required
-        /> */}
-        <div>City: </div>
-        <input
-          name="city"
-          type="text"
-          value={this.state.city}
-          onChange={this.handleChange}
-          required
-        />
-        <div>State: </div>
-        <input
-          name="state"
-          type="text"
-          value={this.state.state}
-          onChange={this.handleChange}
-          required
-        />
-        <div>Zip Code: </div>
-        <input
-          name="zipCode"
-          type="text"
-          value={this.state.zipCode}
-          onChange={this.handleChange}
-          required
-        />
-        <div>Email: </div>
-        <input
-          name="email"
-          type="text"
-          value={this.state.email}
-          onChange={this.handleChange}
-          required
-        />
-        {/* <div>Credit Card: </div>
-        <input
-          name="creditCard"
-          type="text"
-          value={this.state.creditCard}
-          onChange={this.handleChange}
-          required
-        /> */}
-        <Button type="submit">Submit Order</Button>
-      </form>
+      <div className="checkout-container">
+        <Grid container spacing={3} justify="center">
+          <Grid item xs={8}>
+            <Paper>
+              <form onSubmit={this.handleSubmit} className="checkoutForm">
+                <div>
+                  <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="firstName"
+                        name="firstName"
+                        label="First name"
+                        value={this.state.firstName}
+                        onChange={this.handleChange}
+                        fullWidth
+                        autoComplete="fname"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="lastName"
+                        name="lastName"
+                        label="Last name"
+                        fullWidth
+                        value={this.state.lastName}
+                        onChange={this.handleChange}
+                        autoComplete="lname"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="email"
+                        name="email"
+                        label="E-mail"
+                        fullWidth
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        autoComplete="lname"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        id="address1"
+                        name="address1"
+                        label="Address line 1"
+                        fullWidth
+                        value={this.state.address1}
+                        onChange={this.handleChange}
+                        autoComplete="billing address-line1"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        id="address2"
+                        name="address2"
+                        label="Address line 2"
+                        fullWidth
+                        value={item.address2}
+                        onChange={this.handleChange}
+                        autoComplete="billing address-line2"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="city"
+                        name="city"
+                        label="City"
+                        fullWidth
+                        value={this.state.city}
+                        onChange={this.handleChange}
+                        autoComplete="billing address-level2"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        id="state"
+                        name="state"
+                        label="State/Province/Region"
+                        fullWidth
+                        value={this.state.state}
+                        onChange={this.handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="zip"
+                        name="zip"
+                        label="Zip / Postal code"
+                        fullWidth
+                        value={this.state.zipCode}
+                        onChange={this.handleChange}
+                        autoComplete="billing postal-code"
+                      />
+                    </Grid>
+                  </Grid>
+                </div>
+                <Button type="submit" className="form-button">
+                  Submit Order
+                </Button>
+              </form>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
     )
   }
 }
