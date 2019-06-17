8de036207542f396e05ba4afca926522268212d3
Sofia Benitez <sofia.benitez39@gmail.com>
2019-06-17 14:15:39 -0400

    feat: cart is updated from home page

--- client/components/Product.js
+++ client/components/Product.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
-import {addingToCart} from '../store'
+import {addingToCart, updatingCart} from '../store'
 import Button from '@material-ui/core/Button'
 
 const Product = props => {
   const product = props.product
-
+  const cart = props.cart
+  // console.log('PROPS', props);
+  // console.log('CART', cart);
+  let cartItem = cart.filter(el => product.id === el.productId)
+  console.log('CART ITEM', cartItem)
+  let existingQuant = cartItem[0] ? cartItem[0].quantity : 0
+  console.log('EXISISTING QUANT', existingQuant)
   return (
     <div className="card-contents" key={product.key}>
       <Link to={`/products/${product.id}`}>
@@ -24,7 +30,11 @@ const Product = props => {
         <h3>Price: ${product.price / 100}</h3>
         <Button
           variant="contained"
-          onClick={() => props.addToCart(props.user.id, product.id, 1)}
+          onClick={() =>
+            cartItem.length
+              ? props.updateCart(props.user.id, product.id, existingQuant, 1)
+              : props.addToCart(props.user.id, product.id, 1)
+          }
         >
           ADD TO CART
         </Button>
@@ -34,12 +44,16 @@ const Product = props => {
 }
 
 const mapStateToProps = state => ({
-  user: state.user
+  user: state.user,
+  cart: state.cart
 })
 
 const mapDispatchToProps = dispatch => ({
   addToCart: (userId, productId, quantity) => {
     dispatch(addingToCart(userId, productId, quantity))
+  },
+  updateCart: (userId, itemId, quantity, newQuantity) => {
+    dispatch(updatingCart(userId, itemId, quantity, newQuantity))
   }
 })
 
